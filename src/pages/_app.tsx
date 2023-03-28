f35a44ff3ba27d31dc9b111829e0f0f0df36a1e0
shreyas0924 <shreyasp0924@gmail.com>
2023-03-28 07:28:22 +0530

    sign in and sign out done using clerk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,15 +5,18 @@ import { SessionProvider } from "next-auth/react";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <ClerkProvider>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </ClerkProvider>
   );
 };
 
