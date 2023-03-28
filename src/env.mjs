246dd04f033ced58f9bc367bf4cb2890a3398405
shreyas0924 <shreyasp0924@gmail.com>
2023-03-28 06:53:51 +0530

    updated env.mjs

--- src/env.mjs
+++ src/env.mjs
@@ -50,7 +50,7 @@ const processEnv = {
   NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
   DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
-  NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+  // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 };
 
 // Don't touch the part below
