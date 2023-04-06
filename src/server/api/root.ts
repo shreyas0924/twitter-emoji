0e4399d1e7497847dadabf92a5e56500af0ad85a
shreyas0924 <shreyasp0924@gmail.com>
2023-04-06 06:59:33 +0530

    filtered the requried data to the client

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,12 +1,14 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { exampleRouter } from "~/server/api/routers/example";
 import { postsRouter } from "./routers/posts";
+import { profileRouter } from "./routers/profile";
 
 //All routers are present here
 
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
   posts: postsRouter,
+  profile: profileRouter,
 });
 
 // export type definition of API
