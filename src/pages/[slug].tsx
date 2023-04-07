5fe90ee9ef6ba59ff17dc0c769b7f878d3e4ba35
shreyas0924 <shreyasp0924@gmail.com>
2023-04-07 17:59:23 +0530

    prefetch data and profile page

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,16 +1,18 @@
-import { type NextPage } from "next";
+import { GetStaticProps, InferGetStaticPropsType, type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { api } from "~/utils/api";
 import { LoadingPage } from "~/components/Loading";
+import { generateSSGHelper } from "~/server/helpers/ssgHelper";
+import { PageLayout } from "~/components/layout";
+import Image from "next/image";
 
-const ProfilePage: NextPage = () => {
-  const { data, isLoading } = api.profile.getUserByUsername.useQuery({
-    username: "shreyas0924",
+const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
+  const { data } = api.profile.getUserByUsername.useQuery({
+    username,
   });
 
-  if (isLoading) return <LoadingPage />;
   if (!data) return <div>404</div>;
 
   console.log(data);
@@ -18,11 +20,47 @@ const ProfilePage: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Profile</title>
+        <title>{data.username}</title>
       </Head>
-      <main className="flex justify-center">{data.username}</main>
+      <PageLayout>
+        <div className="relative h-36 bg-slate-600">
+          <Image
+            src={data.profile}
+            className=" absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black bg-black"
+            alt={`${data.username} 's profile pic`}
+            width={128}
+            height={128}
+          />
+        </div>
+        <div className="h-[64px]"></div>
+        <div className="p-4 text-xl font-bold">{`@${data.username}`}</div>
+        <div className="w-full border-b border-slate-400"></div>
+      </PageLayout>
     </>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const ssg = generateSSGHelper();
+
+  const slug = context.params?.slug;
+
+  if (typeof slug !== "string") throw new Error("no slug");
+
+  const username = slug.replace("@", "");
+
+  await ssg.profile.getUserByUsername.prefetch({ username });
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      username,
+    },
+  };
+};
+
+export const getStaticPaths = () => {
+  return { paths: [], fallback: "blocking" };
 };
 
 export default ProfilePage;
