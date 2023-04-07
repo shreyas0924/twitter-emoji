0c85fb8d74f9a6d7cfeb30e00d171103a209541f
shreyas0924 <shreyasp0924@gmail.com>
2023-04-07 18:53:31 +0530

    template string

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -41,14 +41,17 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
         <div className="relative h-36 bg-slate-600">
           <Image
             src={data.profile}
-            alt={`${data.username}'s profile pic`}
+            alt={`${data.username}'s profile pic` ?? "profile picture"}
             width={128}
             height={128}
             className="absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black bg-black"
           />
         </div>
         <div className="h-[64px]"></div>
-        <div className="p-4 text-2xl font-bold">{`@${data.username}`}</div>
+        <div className="p-4 text-2xl font-bold">
+          {`@${data.username}` ?? "username"}
+        </div>
+
         <div className="w-full border-b border-slate-400" />
         <ProfileFeed userId={data.id} />
       </PageLayout>
