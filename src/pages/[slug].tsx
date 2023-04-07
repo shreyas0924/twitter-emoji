166a58a2d43041d166b774765c62ba69bca15587
shreyas0924 <shreyasp0924@gmail.com>
2023-04-07 18:50:34 +0530

    slug changes

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -7,7 +7,6 @@ import { generateSSGHelper } from "~/server/helpers/ssgHelper";
 import { PageLayout } from "~/components/layout";
 import Image from "next/image";
 
-
 const ProfileFeed = (props: { userId: string }) => {
   const { data, isLoading } = api.posts.getPostsByUserId.useQuery({
     userId: props.userId,
@@ -42,17 +41,16 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
         <div className="relative h-36 bg-slate-600">
           <Image
             src={data.profile}
-            className=" absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black bg-black"
-            alt={`${data.username} s profile pic`}
+            alt={`${data.username}'s profile pic`}
             width={128}
             height={128}
+            className="absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black bg-black"
           />
         </div>
         <div className="h-[64px]"></div>
-        <div className="p-4 text-xl font-bold">{`@ ${data.username}`}</div>
-        <div className="w-full border-b border-slate-400">
-          <ProfileFeed userId={data.id} />
-        </div>
+        <div className="p-4 text-2xl font-bold">{`@${data.username}`}</div>
+        <div className="w-full border-b border-slate-400" />
+        <ProfileFeed userId={data.id} />
       </PageLayout>
     </>
   );
